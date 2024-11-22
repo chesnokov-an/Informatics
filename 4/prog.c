00d2d55c489e68066769cb85596e2b3b2725a7c3
chesnokov-an <chesnokov-an-2017@mail.ru>
2024-11-22 17:21:44 +0300

    Uploading lab4

---
+++ 4/prog.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+
+#define PROMPT "> "
+
+char *process(const char*);
+
+int main(){
+    char *input = readline(PROMPT);
+    while(input != NULL){
+        printf("input: \"%s\"\n", input);
+        char *output = process(input);
+        printf("output: \"%s\"\n", output);
+        free(input);
+        free(output);
+        input = readline(PROMPT);
+    }
+    
+    return 0;
+}
+
+char *process(const char *input){
+    char *s = strdup(input);
+    int s_len = strlen(s);
+    char *res = calloc(s_len + 2, sizeof(char));
+    int len = 0;
+    char *word = strtok(s, "\t ");
+    while(word != NULL){
+        // printf("Word: \"%s\"\n", word);
+        int w_len = strlen(word);
+        if((w_len % 2) == 0){
+			memcpy(res + len, word, w_len * sizeof(char));
+            len += w_len;
+            res[len] = ' ';
+            len++;
+        }
+		// printf("Result: \"%s\"\n", res);
+		word = strtok(NULL, "\t ");
+	}
+    free(s);
+    if(len > 0){
+        len--;
+    }
+    res[len] = '\0';
+    res = realloc(res, (len + 1) * sizeof(char));
+    return res;
+}
+
+
