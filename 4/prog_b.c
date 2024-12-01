59197884d20384d4e30f27c27ac715b32a8b2899
chesnokov-an <chesnokov-an-2017@mail.ru>
2024-12-01 14:46:44 +0300

    Add my_string functions

--- 4/prog_b.c
+++ 4/prog_b.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #define PROMPT "> "
 
-int my_strlen(char* s);
-char *get_str();
-char *process(char*);
+int my_strlen(const char*);
+char *get_str(const char*);
+char *my_strdup(const char*);
+int my_strin(const char*, char);
+void *my_memcpy(void*, const void*, int);
+char *my_strtok(char*, const char*);
+char *process(const char*);
 
 int main(){
-    char *input = get_str();
+    char *input = get_str(PROMPT);
     while(input != NULL){
         printf("input: \"%s\"\n", input);
         char *output = process(input);
         printf("output: \"%s\"\n", output);
         free(input);
         free(output);
-        input = get_str();
+        input = get_str(PROMPT);
     }
     return 0;
 }
 
-int my_strlen(char* s){
+int my_strlen(const char* s){
 	int res = 0;
 	while(s[res] != '\0'){
 		res += 1;
@@ -28,8 +31,8 @@ int my_strlen(char* s){
 	return res;
 }
 
-
-char *get_str(){
+char *get_str(const char *prompt){
+	printf("%s", prompt);
 	char buf[81] = {0};
 	char *res = NULL;
 	int len = 0;
@@ -46,7 +49,7 @@ char *get_str(){
 			int chunk_len = my_strlen(buf);
 			int new_len = len + chunk_len;
 			res = realloc(res, new_len + 1);
-			memcpy(res + len, buf, chunk_len);
+			my_memcpy(res + len, buf, chunk_len);
 			len = new_len;
 		}
 		if(flag == 0){
@@ -62,7 +65,14 @@ char *get_str(){
 	return res;
 }
 
-char *my_strdup(char *s){
+void *my_memcpy(void *s, const void *ct, int n){
+	for(int i = 0; i < n; i++){
+		*((char*)s + i) = *((char*)ct + i);
+	}
+	return s;
+}
+
+char *my_strdup(const char *s){
 	char *res = NULL;
 	int len = my_strlen(s);
 	res = calloc(len + 1, sizeof(char));
@@ -72,7 +82,7 @@ char *my_strdup(char *s){
 	return res;
 }
 
-int my_strin(char *s, char symbol){
+int my_strin(const char *s, char symbol){
 	int len = my_strlen(s);
 	for(int i = 0; i < len; i++){
 		if(s[i] == symbol){
@@ -82,33 +92,36 @@ int my_strin(char *s, char symbol){
 	return 0;		
 }
 
-char *my_strtok(char *s, char *delim){
-	static char *ptr = NULL;
+char *my_strtok(char *s, const char *delim){
+	static char *ptr = NULL;	
 	if(s != NULL){
 		ptr = s;
 	}
 	if(ptr == NULL){
 		return NULL;
 	}
-	char *res = calloc(my_strlen(ptr) + 1, sizeof(char));
 	int i = 0;
+	while((ptr[i] != '\0') && (my_strin(delim, ptr[i]))){
+		ptr += 1;
+	}
+	char *res = ptr;
 	while(ptr[i] != '\0'){
 		if(!my_strin(delim, ptr[i])){
 			res[i] = ptr[i];
 		}
 		else{
-			res = realloc(res, (i+1) * sizeof(char));
+			ptr[i] = '\0';
 			ptr += i + 1;
 			return res;
-		}	
+		}
 		i++;
 	}
-	res = realloc(res, (i+1) * sizeof(char));
 	ptr = NULL;
 	return res;
 }
 
-char *process(char *input){
+
+char *process(const char *input){
     char *s = my_strdup(input);
     int s_len = my_strlen(s);
     char *res = calloc(s_len + 2, sizeof(char));
@@ -117,7 +130,7 @@ char *process(char *input){
     while(word != NULL){
         int w_len = my_strlen(word);
         if((w_len % 2) == 0){
-			memcpy(res + len, word, w_len * sizeof(char));
+			my_memcpy(res + len, word, w_len * sizeof(char));
             len += w_len;
             res[len] = ' ';
             len++;
